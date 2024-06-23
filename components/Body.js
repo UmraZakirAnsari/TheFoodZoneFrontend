7f43c2189e0561910a218479c52b39926ce9bc3a
UmraZakirAnsari <142561826+UmraZakirAnsari@users.noreply.github.com>
2024-06-23 13:35:41 +0530

    Added proxy url

--- components/Body.js
+++ components/Body.js
@@ -18,7 +18,7 @@ const Body = () => {
   //  const json=body.data;
   //  console.log(json); 
    
-  const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING ");
+  const data= await fetch("https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
   const json=await data.json();
   console.log(json); 
    setlistOfrestaurant(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
